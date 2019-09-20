fbb6bd37732a45ef3ea62e6d4948a79380dc2026
saltandlight <loverdevelop15@gmail.com>
2019-09-20 15:51:42 +0900

    network 공부

--- Network/20190920_network/day04/src/tcp1/Server.java
+++ Network/20190920_network/day04/src/tcp1/Server.java
@@ -11,53 +11,57 @@ public class Server {
 	int port;
 	ServerSocket serverSocket;
 	Socket socket;
-	//네트워크를 하기 위해 양쪽 다 있어야 함 
-	
+	// 네트워크를 하기 위해 양쪽 다 있어야 함
+
 	OutputStream out;
 	OutputStreamWriter osw;
 	BufferedWriter bw;
+
 	
-	public Server(int port) throws IOException{
-		this.port=port;
-		serverSocket= new ServerSocket(port);
-		
+	
+	boolean flag = true;
+
+	public Server(int port) throws IOException {
+		this.port = port;
+		serverSocket = new ServerSocket(port);
+
 	}
-	
+
 	public void startServer() throws IOException {
 		System.out.println("Server Start..");
-		try {
-			System.out.println("Server Ready..");
-			socket = serverSocket.accept();
-			System.out.println("Accepted.."+
-			socket.getInetAddress());
-			
-			out=socket.getOutputStream();
-			osw=new OutputStreamWriter(out);
-			bw = new BufferedWriter(osw);
-			bw.write("hello");
-			
-		}catch(IOException e) {
-			throw e;
-		}finally {
-			if(bw != null) {
-				bw.close();
-			}
-			if(socket != null) {
-				socket.close();
+		while (flag) {
+			try {
+				System.out.println("Server Ready..");
+				socket = serverSocket.accept();
+				System.out.println("Accepted.." + socket.getInetAddress());
+
+				out = socket.getOutputStream();
+				osw = new OutputStreamWriter(out);
+				bw = new BufferedWriter(osw);
+				bw.write("안뇽");
+			} catch (IOException e) {
+				throw e;
+			} finally {
+				if (bw != null) {
+					bw.close();
+				}
+				if (socket != null) {
+					socket.close();
+				}
 			}
 		}
 		System.out.println("Server end..");
 	}
-	
+
 	public static void main(String[] args) {
-		Server server=null;
+		Server server = null;
 		try {
-			server=new Server(8898);
+			server = new Server(8888);
 			server.startServer();
-		
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
